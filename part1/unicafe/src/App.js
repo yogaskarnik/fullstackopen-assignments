90301611bca5166f606a784c24da9f235bbf3405
Yogas Karnik <yogas.karnik@gmail.com>
2023-05-28 17:28:38 +0200

    EX 1.7 step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,12 +4,38 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  const handleGood = () => setGood(good + 1);
+  const handleGood = () => {
+    const updatedGood = good + 1;
+    const updatedTotal = total + 1;
+    const totalAvg = updatedGood - bad;
 
-  const handleBad = () => setBad(bad + 1);
+    setGood(updatedGood);
+    setTotal(updatedTotal);
+    setAverage(totalAvg / updatedTotal);
+    setPositive((updatedGood / updatedTotal) * 100);
+  };
 
-  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => {
+    const updatedBad = bad + 1;
+    const updatedTotal = total + 1;
+    const totalAvg = good - updatedBad;
+
+    setBad(updatedBad);
+    setTotal(updatedTotal);
+    setAverage(totalAvg / updatedTotal);
+  };
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1;
+    const updatedTotal = total + 1;
+
+    setNeutral(updatedNeutral);
+    setTotal(updatedTotal);
+    setAverage(average);
+  };
 
   return (
     <div>
@@ -25,6 +51,12 @@ const App = () => {
         neutral {neutral}
         <br />
         bad {bad}
+        <br />
+        total {total}
+        <br />
+        average {average}
+        <br />
+        positive {positive} %
       </p>
     </div>
   );
