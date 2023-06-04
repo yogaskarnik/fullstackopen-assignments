7499c7d7d8562ca46480b18160e61528ab4fe9ae
Yogas Karnik <yogas.karnik@gmail.com>
2023-06-04 22:34:09 +0200

    EX 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function App() {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addContact = (event) => {
+    event.preventDefault();
+
+    const contact = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    console.log("contact ", contact);
+    setPersons(persons.concat(contact));
+    setNewName("");
+  };
+
+  const handleContactChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input value={newName} onChange={handleContactChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.id}>{person.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
