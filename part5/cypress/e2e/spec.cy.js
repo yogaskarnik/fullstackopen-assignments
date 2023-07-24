c21a660b6e5c1e5017910b42595ed2cb2323d45b
Yogas Karnik <yogas.karnik@gmail.com>
2023-07-24 22:14:53 +0200

    EX 5.20 step4

--- part5/cypress/e2e/spec.cy.js
+++ part5/cypress/e2e/spec.cy.js
@@ -7,31 +7,7 @@ describe('Blog App', function () {
       password: 'salainen',
     }
     cy.request('POST', 'http://localhost:3000/api/users/', user)
-
-    cy.request('POST', 'http://localhost:3000/api/login/', {
-      username: 'ykarnik',
-      password: 'salainen',
-    }).then(({ body }) => {
-      console.log(body)
-      localStorage.setItem('loggedInBlogUser', JSON.stringify(body))
-      const blog = {
-        title: 'Using Cypress for E2E testing',
-        author: 'Yogas Karnik',
-        url: 'http://e2etesting.com',
-        user: body.id,
-      }
-      cy.request({
-        url: 'http://localhost:3000/api/blogs/',
-        method: 'POST',
-        body: blog,
-        headers: {
-          Authorization: `Bearer ${
-            JSON.parse(localStorage.getItem('loggedInBlogUser')).token
-          }`,
-        },
-      })
-    })
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000')
   })
 
   it('front page can be opened', function () {
@@ -77,26 +53,36 @@ describe('Blog App', function () {
 
   describe('When logged in', function () {
     beforeEach(function () {
-      cy.contains('login').click()
-      cy.get('#username').type('ykarnik')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
+      cy.login({ username: 'ykarnik', password: 'salainen' })
+
+      const blog = {
+        title: 'Using Cypress for E2E testing',
+        author: 'Yogas Karnik',
+        url: 'http://e2etesting.com',
+      }
+      cy.createBlog(blog)
     })
 
     it('A blog can be created', function () {
       cy.contains('create new blog').click()
-      cy.get('#title').type('A new blog')
-      cy.get('#author').type('Yogas')
-      cy.get('#url').type('http://yogas.com')
-      cy.get('#create-blog').click()
 
-      cy.get('#blog-list').contains('A new blog Yogas')
+      cy.get('#blog-list').contains('Using Cypress for E2E testing')
 
-      cy.contains('A new blog Yogas').should('exist')
+      cy.contains('Using Cypress for E2E testing').should('exist')
     })
   })
   describe('users can like a blog', function () {
-    it.only('like a blog', function () {
+    beforeEach(function () {
+      cy.login({ username: 'ykarnik', password: 'salainen' })
+
+      const blog = {
+        title: 'Using Cypress for E2E testing',
+        author: 'Yogas Karnik',
+        url: 'http://e2etesting.com',
+      }
+      cy.createBlog(blog)
+    })
+    it('like a blog', function () {
       cy.get('#blog-show').click()
       cy.get('#blog-like').click()
     })
--- part5/cypress/support/commands.js
+++ part5/cypress/support/commands.js
@@ -1,25 +1,25 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3000/api/login/', {
+    username,
+    password,
+  }).then(({ body }) => {
+    localStorage.setItem('loggedInBlogUser', JSON.stringify(body))
+    cy.visit('http://localhost:3000')
+  })
+})
+
+Cypress.Commands.add('createBlog', (blog) => {
+  console.log('user ', JSON.parse(localStorage.getItem('loggedInBlogUser')))
+  cy.request({
+    url: 'http://localhost:3000/api/blogs/',
+    method: 'POST',
+    user: JSON.parse(localStorage.getItem('loggedInBlogUser')).id,
+    body: blog,
+    headers: {
+      Authorization: `Bearer ${
+        JSON.parse(localStorage.getItem('loggedInBlogUser')).token
+      }`,
+    },
+  })
+  cy.visit('http://localhost:3000')
+})
