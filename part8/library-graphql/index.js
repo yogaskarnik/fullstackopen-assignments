0bf8e57fc1d5a4ee4b64e016643838f8b1bda50a
Yogas Karnik <yogas.karnik@gmail.com>
2023-10-04 16:37:31 +0200

    EX 8.5

--- part8/library-graphql/index.js
+++ part8/library-graphql/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String!): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -126,13 +126,18 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
+      let filteredBooks = books;
       if (args.author) {
-        const filteredBooks = books.filter(
-          (book) => book.author === args.author
-        );
-        return filteredBooks || [];
+        filteredBooks = books.filter((book) => book.author === args.author);
+        return filteredBooks;
       }
-      return books;
+      if (args.genre) {
+        filteredBooks = books.filter((book) =>
+          book.genres.includes(args.genre)
+        );
+        return filteredBooks;
+      }
+      return filteredBooks || [];
     },
     allAuthors: () => authors,
   },
