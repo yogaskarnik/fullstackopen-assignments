f7734bd2002a77b2c2e35714ba2ff415c798281f
Yogas Karnik <yogas.karnik@gmail.com>
2023-10-05 17:21:05 +0200

    EX 8.8

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
+import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
+import { __DEV__ } from '@apollo/client/utilities/globals';
+
+const App = () => {
+  const [page, setPage] = useState('authors');
+
+  if (__DEV__) {
+    loadDevMessages();
+    loadErrorMessages();
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors show={page === 'authors'} />
+
+      <Books show={page === 'books'} />
+
+      <NewBook show={page === 'add'} />
+    </div>
+  );
+};
+
+export default App;
